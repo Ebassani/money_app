e5e5ed74481015518e68611480ef1b32cd8e79d4
Eduardo <eduardo.sbassani@gmail.com>
2023-10-10 22:11:02 +0300

    Aquiring and displaying data for expenses in ExpensesView

--- components/expenses.tsx
+++ components/expenses.tsx
@@ -13,27 +13,56 @@ import {
     View,
   } from 'react-native';
 
-import { read, deleteExpenses, changeExpenseWallet, getExpenseType } from '../database/database';
+import { read, deleteExpenses, changeExpenseWallet, getExpenseType, getWalletName } from '../database/database';
 
 export const ViewExpenses = () => {
     const [Expenses,SetExpenses]=useState([]);
 
-    async function readExpenses () {
+    async function readExpenses() {
         await  read('expenses')
         .then((result) => {
             SetExpenses(result);
         });
     
-        Expenses.forEach((element: any) => {
-            getExpenseType(element.expense_type_id).then(result=> {console.log(result);})
-        });
     }
-
+    
     readExpenses()
     
-
-    //console.log(Expenses);
-    return <Text>Hello, Hello component</Text>
+    return ( 
+        <View>
+            <ScrollView>
+                {Expenses.map((item, index)=>{
+                return <PriceExpense expense={item} key={index} />
+                })}
+            </ScrollView>
+        </View>
+    )
 }
 
+const PriceExpense = (data: any, key: any) => {
+    const [Type,SetType]=useState('');
+    const [Wallet,SetWallet]=useState('');
 
+    const expense = data.expense
+
+    async function readValues() {
+        await getExpenseType(expense.expense_type_id).then(type => { 
+            SetType(type);
+        });
+        await getWalletName(expense.wallet_id).then(wallet => SetWallet(wallet));
+    }
+    
+    readValues()
+
+    return (
+        <View key={key}>
+            <View>
+                <Text>{Type}</Text>
+                <Text>{Wallet}</Text>
+            </View>
+            <View>
+                <Text>{expense.amount}</Text>
+            </View>
+        </View>
+    );
+}
